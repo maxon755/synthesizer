03016d0dd1b82c28e366fd00302ea895219753af
Maksym Haiduk <maksym.haiduk@gmail.com>
2023-03-04 13:07:54 +0200

    Implement instrument changing

--- src/main/java/org/openjfx/App.java
+++ src/main/java/org/openjfx/App.java
@@ -5,6 +5,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import org.openjfx.synthesizer.Synthesizer;
 
 /**
  * JavaFX App
@@ -16,10 +17,14 @@ public class App extends Application {
         var javaVersion = SystemInfo.javaVersion();
         var javafxVersion = SystemInfo.javafxVersion();
 
+        Synthesizer synthesizer = new Synthesizer();
+
         var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
         var scene = new Scene(new StackPane(label), 640, 480);
         stage.setScene(scene);
         stage.show();
+
+        synthesizer.playNote();
     }
 
     public static void main(String[] args) {
--- src/main/java/org/openjfx/synthesizer/Synthesizer.java
+++ src/main/java/org/openjfx/synthesizer/Synthesizer.java
@@ -1,12 +1,44 @@
 package org.openjfx.synthesizer;
 
+import javax.sound.midi.*;
+
 public class Synthesizer {
 
-    Synthesizer() {
+    private javax.sound.midi.Synthesizer synthesizer;
 
+    private MidiChannel midiChannel;
+
+    private Instrument currentInstrument;
+
+    public Synthesizer() {
+        try {
+            synthesizer = MidiSystem.getSynthesizer();
+            synthesizer.open();
+
+            Instrument[] instruments = synthesizer.getLoadedInstruments();
+            midiChannel = synthesizer.getChannels()[0];
+            setCurrentInstrument(instruments[0]);
+        } catch (MidiUnavailableException e) {
+            System.out.println("Cannot get synthesizer");
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
     }
 
     public void playNote() {
+        midiChannel.noteOn(60, 80);
+    }
 
+    public Instrument[] getInstruments() {
+        return synthesizer.getLoadedInstruments();
+    }
+
+    public Instrument getCurrentInstrument() {
+        return currentInstrument;
+    }
+
+    public void setCurrentInstrument(Instrument instrument) {
+        midiChannel.programChange(instrument.getPatch().getProgram());
+        currentInstrument = instrument;
     }
 }
